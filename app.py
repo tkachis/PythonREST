748cc0e61fb3a06822260f58ab438e5c5fdea060
Igor Tkachev <tkacheviscoder@gmail.com>
2019-12-02 14:47:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import identity, authenticate
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+api = Api(app)
+
+# Создание БД перед первым запросом на основании данных из него
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth => {POST}
+api.add_resource(UserRegister, '/register')  # /register => {POST}
+# /item/<string:name> => {GET, POST, PUT, DELETE}
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')  # /items => {GET}
+# /store/<string:name> => {GET, POST, DELETE}
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')  # /stores => {GET}
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
